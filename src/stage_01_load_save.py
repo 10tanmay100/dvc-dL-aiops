914c1781a449dd98a9e6e65c3cea3f14d6645729
Tanmay Chakraborty <chakrabortytanmay326@gmail.com>
2022-06-24 10:09:48 +0530

    config file has been added and setup.py installed and data stored

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -27,7 +27,7 @@ def get_data(config_path):
 
     for source_download_dir,local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs),total=2,desc="List of folders"):
         create_directory([local_data_dir])
-        copy_file()
+        copy_file(source_download_dir,local_data_dir)
 
 
 
