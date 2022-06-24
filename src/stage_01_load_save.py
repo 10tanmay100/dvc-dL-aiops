aa0afa56a93213eba5547f3c33f66a675a839ce8
Tanmay Chakraborty <chakrabortytanmay326@gmail.com>
2022-06-24 10:02:26 +0530

    stage1 added changes

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -2,24 +2,32 @@ from src.utils.all_utils import read_yaml,create_directory
 import argparse
 import pandas as pd
 import os
+import shutil
+from tqdm import tqdm
+
+
+def copy_file(source_download_dir,local_data_dir):
+    list_of_files=os.listdir(source_download_dir)
+    N=len(list_of_files)
+    for file in tqdm(list_of_files,total=N,desc=f"copying file from {source_download_dir} to {local_data_dir}",colour="green"):
+        src=os.path.join(source_download_dir,file)
+        dest=os.path.join(local_data_dir,file)
+        shutil.copy(src,dest)
+
+
+
 
 def get_data(config_path):
     config=read_yaml(config_path)
-    df=pd.read_csv(config["data_source"],delimiter=";")
-    #creating directory from config files for files to save it in local
-    artifacts_dir=config["artifacts"]["artifcats_dir"]
-    raw_local_dir=config["artifacts"]["raw_local_dir"]
-    raw_local_file=config["artifacts"]["raw_local_file"]
-    #create a directory
-    raw_local_dir_path=os.path.join(artifacts_dir,raw_local_dir)
-    #adding data in csv directory
-    create_directory(dirs=[raw_local_dir_path])
-    #creating csv file for the data
-    raw_local_file_path=os.path.join(raw_local_dir_path,raw_local_file)
-    #now convert to csv
-    df.to_csv(raw_local_file_path,sep=",",index=False)
+
+    source_download_dirs=config["source_download_dirs"]
+
+    local_data_dirs=config["local_data_dirs"]
 
 
+    for source_download_dir,local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs),total=2,desc="List of folders"):
+        create_directory([local_data_dir])
+        copy_file()
 
 
 
